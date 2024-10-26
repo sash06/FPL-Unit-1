2fc05f6b1b5982f1470a5a8bb03362bfdbe96267
sash06 <sashreekgokhale@gmail.com>
2024-10-26 22:48:17 +0530

    Add files via upload

---
+++ Variables define.c
@@ -0,0 +1,10 @@
+// Code for variable define.
+#include<stdio.h>
+int main()
+{
+    int a=10;    // integer variable
+    char b;     // character variable
+    float r=4.567;  // float variables
+   
+    return 0;
+}
