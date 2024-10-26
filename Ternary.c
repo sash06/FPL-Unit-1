c83d2296cc6d68f4ea42f4fd506066ed7d1c179d
sash06 <sashreekgokhale@gmail.com>
2024-10-26 22:23:08 +0530

    Add files via upload

---
+++ Ternary.c
@@ -0,0 +1,11 @@
+// Code for ternary operator:-
+#include<stdio.h>
+int main()
+{
+    // We have to find maximium value by ternary operator.
+    int a=10,b=45,max;
+    max=(a>b)?a:b;
+    printf("max=  %d",max);
+    return 0;
+
+}
---
+++ if else.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=38,b=47;
+    if(a>b)
+    printf("\n a is greater than b");
+    else
+    printf("b is greater than a");  
+    return 0;
+}
