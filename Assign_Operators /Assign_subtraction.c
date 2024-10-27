2aacca1583e4c5b44f2e9e175f1c97ae1ec206fc
sash06 <sashreekgokhale@gmail.com>
2024-10-27 15:23:22 +0530

    Create Assign_subtraction.c

---
+++ Assign_Operators /Assign_subtraction.c
@@ -0,0 +1,10 @@
+//Assignment Operator:- 2)Code for Assignment operator.
+#include <stdio.h>
+int main()
+{
+    int a=24;
+    (a-=7);
+    printf("\n the Value of a is: %d",a);
+    return 0;
+
+}
